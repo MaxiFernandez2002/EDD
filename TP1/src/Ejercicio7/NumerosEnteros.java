18008322a5f519d02f098914b0994bde12cea452
Tomas Agustin Arroyo <arroyotomas1999@gmail.com>
2022-08-29 11:28:38 -0300

    terminado ejercicio 7

--- TP1/src/Ejercicio7/NumerosEnteros.java
+++ TP1/src/Ejercicio7/NumerosEnteros.java
@@ -19,5 +19,28 @@ public class NumerosEnteros {
 		}
 		return toReturn;
 	}
-
+	
+	public boolean estaNum(int n) {
+		boolean toReturn = false;
+		if (cant > 0) {
+			toReturn = estaNumRec(n, 0);
+		}
+		else {
+			System.out.println("El arreglo esta vacio");
+		}
+		return toReturn;
+	}
+	
+	private boolean estaNumRec(int n, int i) {
+		boolean toReturn = false;
+		if(i<cant) {
+			if (n == datos[i]) {
+				toReturn = true;
+			}
+			else {
+				estaNumRec(n, i+1);
+			}
+		}
+		return toReturn;
+	}
 }
